5f6f84ec4f8f1dc3d7e536becf0570c17a430fd8
Anuj Pandey <pandey.anujk@gmail.com>
2018-06-03 00:48:48 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -10,11 +10,12 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  return thisIsCrazy();
+  
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  return thisIsCrazy();
 }
 
 function sayMyName() {
