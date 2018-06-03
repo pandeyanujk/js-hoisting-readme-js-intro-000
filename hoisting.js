dc73cf7a22d3f4755537448e5933a572191d89c8
Anuj Pandey <pandey.anujk@gmail.com>
2018-06-03 00:51:37 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -21,11 +21,9 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
